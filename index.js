2f09f3d5e43229de4cac1d99c643e8c2d7f9a86e
Gediminas M <gediminas.mikelenas@nortal.com>
2024-02-18 13:33:14 +0200

    Add animated boxes

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".box").forEach(function (box) {
+  box.addEventListener("click", function () {
+    const animations = ["crawl", "dangle", "jump", "stretch", "flip"];
+    const animation = animations[Math.floor(Math.random() * 5)];
+
+    box.classList.add(animation);
+
+    setTimeout(function () {
+      box.classList.remove(animation);
+    }, 4000);
+  });
+});
